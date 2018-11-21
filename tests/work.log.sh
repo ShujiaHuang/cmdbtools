52031236e1c4a97a9669d233bd83b0dad5d0f075
Shujia Huang <huangshujia@huangshujias-iMac.local>
2018-11-21 08:00:42 +0800

    update

--- tests/work.log.sh
+++ tests/work.log.sh
@@ -1,3 +1,1 @@
-rsync -e ssh huangshujia@10.225.3.18:/zfssz4/BC_RD_P2/PROJECT/F16ZQSI1SH2595/ifs2_BC_RD_P2/huangshujia/20171022_40WGS/output/40samples_vcf/t.vcf.gz ./
-rsync -e ssh huangshujia@10.225.3.18:/zfssz4/BC_RD_P2/USER/huangshujia/other/cmdb_transfer_data/genome/cmdb.test.vcf.gz ./
 time python ../cmdbtools/cmdbtools.py annotate -i cmdb.test.vcf.gz
