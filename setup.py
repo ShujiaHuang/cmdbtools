b0843dec3f42e96d0d866e71c0f628b2f0f66619
Shujia Huang <huangshujia@Shujias-MacBook-Air.local>
2018-12-10 14:29:03 +0800

    add real path for README.rst as a long discript

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 Version 1.0.0 (Nov 22, 2018)
 Copyright (c) 2018 Shujia Huang
 """
+import os
+import sys
+
 try:
     from setuptools import setup, find_packages
     _has_setuptools = True
@@ -21,6 +24,8 @@ VERSION = "1.0.3"
 
 
 if __name__ == "__main__":
+
+    long_description = os.path.split(os.path.realpath(__file__))[0] + "/README.rst"
     setup(name=DISTNAME,
           version=VERSION,
           author=MAINTAINER,
@@ -28,7 +33,7 @@ if __name__ == "__main__":
           maintainer=MAINTAINER,
           maintainer_email=MAINTAINER_EMAIL,
           description=DESCRIPTION,
-          long_description=(open("README.rst").read()),
+          long_description=(open(long_description).read()),
           license=LICENSE,
           url=URL,
           download_url=DOWNLOAD_URL,
