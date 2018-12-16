6abeddccd79d66c1d6543e7b7b918c0b5b3f7734
Shujia Huang <huangshujia@Shujia-Huangs-iMac.local>
2018-12-16 12:53:34 +0800

    modify

--- setup.py
+++ setup.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
               'Operating System :: POSIX',
               'Operating System :: POSIX :: Linux',
               'Operating System :: MacOS'],
-          )
+    )
