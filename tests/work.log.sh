ca6852d2ac758e663a14c16aa13772f30bec3618
Shujia Huang <huangshujia@Shujias-MacBook-Air.local>
2020-09-08 15:19:45 +0800

    update

--- tests/work.log.sh
+++ tests/work.log.sh
@@ -1,2 +1,3 @@
 time python ../cmdbtools/cmdbtools.py annotate -i cmdb.test.vcf.gz
 time python ../cmdbtools/cmdbtools.py query-variant -c 22 -p 46616520 -l positions.list
+time python ../cmdbtools/cmdbtools.py query-variant -c 17 -p 41191488
